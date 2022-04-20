a3af18f7104e57f78d6a693617ae051c43914bc3
João Almeida e Sousa <53518061+JSousa-UoL@users.noreply.github.com>
2022-04-20 11:18:29 +0100

    try/except in _init__ to avoid crashing if offline

--- dbdicom/__init__.py
+++ dbdicom/__init__.py
@@ -1,12 +1,16 @@
 import base64
 import requests
 url = 'https://api.github.com/repos/QIB-Sheffield/dbdicom/contents/README.md'
-req = requests.get(url)
-if req.status_code == requests.codes.ok:
-    req = req.json()  # the response is a JSON
-    content = base64.b64decode(req['content'])
-    introduction = content.decode()
-    __doc__ = introduction
+try:
+    req = requests.get(url)
+    if req.status_code == requests.codes.ok:
+        req = req.json()  # the response is a JSON
+        content = base64.b64decode(req['content'])
+        introduction = content.decode()
+        __doc__ = introduction
+except: # if there's no internet connection
+    pass
+
 
 # do not show in documentation
 __pdoc__ = {}
